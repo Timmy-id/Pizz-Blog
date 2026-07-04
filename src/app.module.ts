ae5b6c14c2995b47248357425e13745db8295f41
Oluwatimilehin Idowu <oluwatimilehin.id@gmail.com>
2026-07-04 11:35:19 +0100

    chore: first commit

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+/* eslint-disable prettier/prettier */
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TagsModule } from './tags/tags.module';
+
+@Module({
+  imports: [TagsModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
