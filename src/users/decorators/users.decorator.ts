04349789ff60ccc2bd3757277e233af177a15da5
Oluwatimilehin Idowu <oluwatimilehin.id@gmail.com>
2026-07-10 08:51:59 +0100

    feat: Create the user decorator

---
+++ src/users/decorators/users.decorator.ts
@@ -0,0 +1,21 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { UsersEntity } from '../users.entity';
+
+export const User = createParamDecorator((data: any, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest()
+    const user: UsersEntity = request.user
+
+    if (!user) {
+        return null
+    }
+
+    if (data) {
+        return user[data]
+    }
+
+    return user
+})
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,15 +1,17 @@
 /* eslint-disable prettier/prettier */
-import { Controller, Get, Req } from '@nestjs/common';
+/* eslint-disable @typescript-eslint/no-unnecessary-type-assertion */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+import { Controller, Get } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { IUserResponse } from './types/usersResponse.interface';
-import { AuthRequest } from '../types/expressRequest.interface';
+import { User } from './decorators/users.decorator';
 
 @Controller('users')
 export class UsersController {
     constructor(private readonly usersService: UsersService) {}
 
     @Get('me')
-    getCurrentUser(@Req() req: AuthRequest): IUserResponse {
-        return this.usersService.generateUserResponse(req.user!)
+    getCurrentUser(@User() user): IUserResponse {
+        return this.usersService.generateUserResponse(user!)
     }
 }
