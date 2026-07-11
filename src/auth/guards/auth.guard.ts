ea31e8773090271ffd7d48235bf21fb35a069f51
Oluwatimilehin Idowu <oluwatimilehin.id@gmail.com>
2026-07-11 19:05:26 +0100

    feat: Create the auth guard

---
+++ src/auth/guards/auth.guard.ts
@@ -0,0 +1,18 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+import { AuthRequest } from '@/types/expressRequest.interface';
+import { CanActivate, ExecutionContext, HttpException, HttpStatus, Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  canActivate(context: ExecutionContext): boolean {
+
+    const request = context.switchToHttp().getRequest<AuthRequest>()
+
+    if (request.user && request.user.id) {
+        return true
+    }
+    throw new HttpException("Unauthorized", HttpStatus.UNAUTHORIZED)
+  }
+}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,16 +1,18 @@
 /* eslint-disable prettier/prettier */
 /* eslint-disable @typescript-eslint/no-unnecessary-type-assertion */
 /* eslint-disable @typescript-eslint/no-unsafe-argument */
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { IUserResponse } from './types/usersResponse.interface';
 import { User } from './decorators/users.decorator';
+import { AuthGuard } from '@/auth/guards/auth.guard';
 
 @Controller('users')
 export class UsersController {
     constructor(private readonly usersService: UsersService) {}
 
     @Get('me')
+    @UseGuards(AuthGuard)
     getCurrentUser(@User() user): IUserResponse {
         return this.usersService.generateUserResponse(user!)
     }
